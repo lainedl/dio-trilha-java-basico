45c7904036f51c720db6381ba8738d32a649a1d8
Edilaine Dias Lima <layne.dl@gmail.com>
2023-03-01 19:01:32 -0300

    Inclusão de documentação Java

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+
+    public static void main(String[] args){
+        //Olá, eu sou um comentário de uma única linha
+
+        /*Olá, eu sou um comentário
+         * que posso ser mais detalhado 
+         * quando necessário 
+         */
+    }
+         /**
+          * Estas duas estrelinhas acima é para identificar que você 
+          * pretende elaborar um comentário a nivel de documentação.
+          * Que incrível !!!
+          */
+          public void metodo(){
+          }
+
+          //javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+   
+}
